27e868341c52558ed4e4fba171b6c6acc6a295d5
ukorvl <ukorotovskiy@gmail.com>
2022-12-26 12:32:39 +0400

    add internal/assignRefs function

---
+++ lib/internal/assignRefs.ts
@@ -0,0 +1,18 @@
+import { MutableRefObject, Ref } from 'react';
+
+/**
+ * Takes both ref objects and ref functions and returns assigned ref function.
+ * @param refs Refs to assign.
+ * @returns Assigned ref.
+ */
+export const assignRefs = <T extends unknown>(...refs: Ref<T | null>[]) => {
+  return (node: T | null) => {
+    refs.forEach(r => {
+      if (typeof r === 'function') {
+        r(node);
+      } else if (r) {
+        (r as MutableRefObject<T | null>).current = node;
+      }
+    });
+  };
+};
